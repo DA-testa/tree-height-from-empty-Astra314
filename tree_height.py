107ae1207d15fde52680a48e14f176e33d0d51c5
Julija Gaidelene 211RDB333 <109692652+Astra314@users.noreply.github.com>
2023-03-02 19:39:41 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,18 +1,18 @@
 # python3
 
-import sys
-import threading
-import numpy
+#import sys
+#import threading
+#import numpy
 
 
-def compute_height(n, parents):
+#def compute_height(n, parents):
     # Write this function
-    max_height = 0
+#    max_height = 0
     # Your code here
-    return max_height
+#    return max_height
 
 
-def main():
+#def main():
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
@@ -21,13 +21,33 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    pass
+#    pass
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
-main()
+#sys.setrecursionlimit(10**7)  # max depth of recursion
+#threading.stack_size(2**27)   # new thread will get stack of such size
+#threading.Thread(target=main).start()
+#main()
 # print(numpy.array([1,2,3]))
+
+def height(tree):
+    depth_list = [1]*len(tree)
+    for i, n in enumerate(tree):
+        while n != -1:
+            depth_list[i] += 1
+            n = tree[n]
+    #print(depth_list)
+             
+    return depth_list
+
+nodes=int(input())
+parents = input().strip()
+
+lst=[int(x) for x in parents.split(" ")]
+print(lst)
+if nodes==len(lst) and max(lst)<=nodes-1:
+    print(max(height(lst)))
+else:
+    print("input error")
